9a547b1d2212135b17f9c2c0b0d6c62ddb6ff13b
AnishChhetry <anishchhetry2013@gmail.com>
2025-02-21 13:55:58 +0530

    anish1

---
+++ Anish_Chhetry_Git_GoLang/assignment1/q1/a1.go
@@ -0,0 +1,13 @@
+package main
+
+func fizzbuzz(n int) string {
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
+		return "Fizz"
+	} else if n%5 == 0 {
+		return "Buzz"
+	} else {
+		return "None"
+	}
+}
