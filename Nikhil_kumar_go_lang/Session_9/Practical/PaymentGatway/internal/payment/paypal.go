8261e666adfa9a97385844b29dfdd8f7bab2b675
NikhilCyberk <111166860+NikhilCyberk@users.noreply.github.com>
2025-03-13 11:19:02 +0530

    sessionn_9_practical_payment_gateway

---
+++ Nikhil_kumar_go_lang/Session_9/Practical/PaymentGatway/internal/payment/paypal.go
@@ -0,0 +1,22 @@
+package payment
+
+import "fmt"
+
+
+type PayPal struct {
+	ClientID   string
+	APIKey     string
+	MerchantID string
+}
+
+func (p PayPal) Pay(amount float64) string {
+	return fmt.Sprintf("Paid $%.2f using PayPal", amount)
+}
+
+func (p PayPal) Refund(transactionID string) string {
+	return fmt.Sprintf("Refund issued for transaction %s via PayPal", transactionID)
+}
+
+func (p PayPal) GetProviderName() string {
+	return "PayPal"
+}
